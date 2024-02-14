6a76e8b514c32e2addc55a334bd76aad9a2015e6
CoderEvoke <CodersEvoke72@gmail.com>
2024-02-14 15:05:16 +0500

    pokemon whole project done all the source code is here

--- App.tsx
+++ App.tsx
@@ -1,118 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {  StyleSheet, SafeAreaView ,Platform, ScrollView } from 'react-native'
+import React from 'react'
+import PokemonCard from './Components/PokemonCard'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const App = () => {
+  const charmanderData ={
+    name: 'Charmander',
+    image: require('./assests/charmander.png'),
+    type: 'Fire',
+    hp: 39,
+    moves: ['Scratches','Ember','Growl','Leer'],
+    weakness: ['Water', 'Ground'],
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const squirtleData = {
+  name: "Squirtle",
+  image: require("./assests/squirtle.png"), 
+  type: "Water",
+  hp: 44,
+  moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+  weakness: ["Electric", "Grass"],
+};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const bulbasaurData = {
+  name: "Bulbasaur",
+  image: require("./assests/bulbasaur.png"),
+  type: "Grass",
+  hp: 45,
+  moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+  weakness: ["Fire", "Ice", "Flying", "Psychic"],
+};
+
+const pikachuData = {
+  name: "Pikachu",
+  image: require("./assests/pikachu.png"), 
+  type: "Electric",
+  hp: 35,
+  moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+  weakness: ["Ground"],
+};
+
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <PokemonCard {...charmanderData}/>
+      <PokemonCard {...squirtleData}/>
+      <PokemonCard {...bulbasaurData}/>
+      <PokemonCard {...pikachuData}/>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+export default App
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  }
+})
